c98cc38e3cd7723c32276723e8532824cf03c448
lemasani <brianlemasan@gmail.com>
2026-02-19 11:48:32 +0300

    Replace Vite branding with LaunchStack/React

--- packages/launchstack/template/base/src/App.tsx
+++ packages/launchstack/template/base/src/App.tsx
@@ -1,8 +1,13 @@
+import reactlogo  from '@/assets/react.svg'
 export function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-background">
+    <div className="min-h-screen flex items-center justify-center bg-[#1e1b4b]">
       <div className="text-center">
-        <h1 className="text-4xl font-bold mb-4">Welcome to React + Vite</h1>
+        <div className="flex items-center justify-between mb-20">
+          <img src="./launchstack_icon.svg" alt="LaunchStack Icon" className="w-32 h-32" />
+          <img src={reactlogo} alt="React Logo" className="w-32 h-32" />
+        </div>
+        <h1 className="text-4xl text-primary font-bold mb-4">Welcome to LaunchStack + React</h1>
         <p className="text-muted-foreground">
           Edit <code className="bg-muted px-2 py-1 rounded">src/App.tsx</code> and save to reload.
         </p>
